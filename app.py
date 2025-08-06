02ed651534849e60132962c0fa613a7b0830ac0e
hyronguyen <hothanhgiang07@gmail.com>
2025-08-06 16:32:50 +0700

    add v2 model

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
-from randomPredict import predict
+from randomPredict import predict, predict2
 
 app = Flask(__name__)
 
 # Đăng ký route từ tệp predict.py
 app.add_url_rule('/api/predict', 'predict', predict, methods=['POST'])
+app.add_url_rule('/api/predict2', 'predict2', predict2, methods=['POST'])
 
 if __name__ == '__main__':
     app.run(debug=True)
--- randomPredict.py
+++ randomPredict.py
@@ -6,6 +6,9 @@ import pandas as pd
 MODEL_PATH = 'model/rf_model_ori.joblib'
 model = joblib.load(MODEL_PATH)
 
+MODEL_PATH2 = 'model/rf_model_v2.joblib'
+model2 = joblib.load(MODEL_PATH2)
+
 # Các feature yêu cầu mà mô hình yêu cầu
 required_features = ['V1', 'V2', 'V3', 'V4', 'V7', 'V9', 'V10', 'V11', 'V12', 'V14', 'V16', 'V17', 'V18', 'Amount']
 
@@ -38,3 +41,35 @@ def predict():
     except Exception as e:
         print(f"Error during prediction: {str(e)}")
         return jsonify({'error': f"Prediction error: {str(e)}"}), 500
+    
+
+
+def predict2():
+    try:
+        data = request.get_json()
+
+        features = data.get('features')
+
+        if not features or not isinstance(features, list):
+            return jsonify({'error': 'Missing or invalid features. It should be a list.'}), 400
+
+        if len(features) != len(required_features):
+            return jsonify({'error': f"Expected {len(required_features)} features, but got {len(features)}."}), 400
+
+        features_df = pd.DataFrame([features], columns=required_features)
+
+        prediction = model2.predict(features_df)  
+        probas = model2.predict_proba(features_df)  
+
+        gianLan = prediction[0].item() 
+        xacXuat = probas[0][1].item()  
+        ghiChu = "Dự đoán gian lận với xác suất cao" if gianLan == 1 else "Không gian lận" 
+
+        return jsonify({
+            'gianLan': gianLan,
+            'xacXuatDuDoan': xacXuat*100,
+            'ghiChu': ghiChu 
+        })
+    except Exception as e:
+        print(f"Error during prediction: {str(e)}")
+        return jsonify({'error': f"Prediction error: {str(e)}"}), 500    
